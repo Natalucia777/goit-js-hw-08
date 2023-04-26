a47fdb1682e717503770387c80c0a6d0bde49ec0
Natalya Demidenko <natalucia777@ukr.net>
2023-04-27 00:29:27 +0300

    hw-8-1-2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+//1, 3
+const galleryContent = document.querySelector('ul.gallery');
+function markup(items) {
+  return items.map(({ preview, original, description }) => {
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+              <img class="gallery__img" src="${preview}" data-source="${original}" 
+              alt="${description}" /></a></li>`; }).join('');}
+galleryContent.innerHTML = markup(galleryItems);
+//4
+galleryContent.addEventListener('click', onGetImage);
+function onGetImage(e) { e.preventDefault();
+  const { target } = e;
+  if (!target.classList.contains('gallery__img')) {
+    return;
+  }
+//5
+  const instance = basicLightbox.create(`<img src="${target.dataset.source}"/>`, {
+    onShow: () => window.addEventListener('keydown', onEscButton),
+    onClose: () => window.removeEventListener('keydown', onEscButton), });
+  instance.show();
+  function onEscButton(e) {
+    if (e.code === 'Escape') { instance.close(); }
+  }
+}
