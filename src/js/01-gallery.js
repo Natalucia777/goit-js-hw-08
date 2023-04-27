d2f3bf6954982696b1df9dd40d7689624d48118f
Natalya Demidenko <natalucia777@ukr.net>
2023-04-28 00:54:13 +0300

    hw-8-2-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,7 @@
-// Add imports above this line
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
-//1, 3
 const galleryContent = document.querySelector('ul.gallery');
 function markup(items) {
   return items.map(({ preview, original, description }) => {
@@ -11,14 +9,14 @@ function markup(items) {
               <img class="gallery__img" src="${preview}" data-source="${original}" 
               alt="${description}" /></a></li>`; }).join('');}
 galleryContent.innerHTML = markup(galleryItems);
-//4
+
 galleryContent.addEventListener('click', onGetImage);
 function onGetImage(e) { e.preventDefault();
   const { target } = e;
   if (!target.classList.contains('gallery__img')) {
     return;
   }
-//5
+
   const instance = basicLightbox.create(`<img src="${target.dataset.source}"/>`, {
     onShow: () => window.addEventListener('keydown', onEscButton),
     onClose: () => window.removeEventListener('keydown', onEscButton), });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,5 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 const iframe = document.querySelectorAll('#vimeo-player');
 const vimeoPlayer = new player(iframe);
+vimeoPlayer.on('timeupdate');
