aaa419e878deb4041676adf9d471367a3b33b857
Natalucia777 <natalucia777@ukr.net>
2023-04-27 21:52:58 +0000

    Deploying to gh-pages from @ Natalucia777/goit-js-hw-08@d2f3bf6954982696b1df9dd40d7689624d48118f 🚀

--- 02-video.52ed52a0.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelectorAll("#vimeo-player");new player(e)}();
-//# sourceMappingURL=02-video.52ed52a0.js.map
--- 02-video.8b07214f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelectorAll("#vimeo-player");new player(e);
-//# sourceMappingURL=02-video.8b07214f.js.map
