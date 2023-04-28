d1668f9e347ff9d91e2a618a545ac57a0b754685
Natalya Demidenko <natalucia777@ukr.net>
2023-04-28 23:08:22 +0300

    hw-8-3-7

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,20 +4,32 @@ var throttle = require('lodash.throttle');
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
+  textarea: document.querySelector('.feedback-form textarea'), };
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
-
-
+//refs.textarea.addEventListener('input', onTextareaInput);
+populateTextarea();
+function populateTextarea() {
+  const sevedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (sevedMessage) {
+    refs.input.value = sevedMessage.email;
+    refs.textarea.value = sevedMessage.message;
+  }
+}
 const LOCAL_STORAGE_KEY = "feedback-form-state";
 function onFormSubmit(evt) {
   evt.preventDefault();
   evt.currentTarget.reset();
   console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
-  localStorage.removeItem(LOCAL_STORAGE_KEY); }
-function onTextareaInput(evt) {
-  const message = evt.currentTarget.value;
-  console.log(message);
-  localStorage.setItem('feedback-form-state', message);
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
+  
+
+refs.form.addEventListener('input', throttle(onTextareaInput(), 500)) ;
+function onTextareaInput(evt) {
+  const dataMessage = new FormData(evt.currentTarget);
+  let localStorageData = {};
+  dataMessage.forEach((value, name) => {
+      const obj = { [name]: value, };
+      Object.assign(localStorageData, obj); });
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localStorageData)); } 
+
