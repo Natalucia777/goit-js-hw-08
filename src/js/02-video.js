17bbf39e1c70c045e90b9b39f8e44378c9a0a1a9
Natalya Demidenko <natalucia777@ukr.net>
2023-04-28 21:07:52 +0300

    hw-8-3-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,8 +2,8 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const framePlayer = document.querySelectorAll('#vimeo-player');
-const vimeoPlayer = new player(framePlayer);
-vimeoPlayer.on( 'timeupdate', throttle(function ({seconds}) {
+const vimeoPlayer = new Player(framePlayer);
+vimeoPlayer.on( 'timeupdate', throttle(function ({ seconds }) {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(Math.round(seconds)) );
     console.log('played the video!', Math.round(seconds)); }, 1000));
 vimeoPlayer.setCurrentTime( JSON.parse(localStorage.getItem('videoplayer-current-time')) );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,15 @@
 import throttle from 'lodash.throttle';
-const throttle = require('lodash.throttle');
+var throttle = require('lodash.throttle');
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('feedback-form textarea'),
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+function onFormSubmit(evt) {
+  
+}
+function onTextareaInput(evt) {
+  
+}
