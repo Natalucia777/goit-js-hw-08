4e2e148d5e950a3968de358253377191df3f4167
Natalya Demidenko <natalucia777@ukr.net>
2023-04-28 23:43:18 +0300

    hw-8-3-8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,11 @@
 import throttle from 'lodash.throttle';
 var throttle = require('lodash.throttle');
-
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form input'),
   textarea: document.querySelector('.feedback-form textarea'), };
 refs.form.addEventListener('submit', onFormSubmit);
-//refs.textarea.addEventListener('input', onTextareaInput);
 populateTextarea();
 function populateTextarea() {
   const sevedMessage = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
@@ -15,21 +14,20 @@ function populateTextarea() {
     refs.textarea.value = sevedMessage.message;
   }
 }
-const LOCAL_STORAGE_KEY = "feedback-form-state";
 function onFormSubmit(evt) {
   evt.preventDefault();
   evt.currentTarget.reset();
   console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
   localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
-  
-
-refs.form.addEventListener('input', throttle(onTextareaInput(), 500)) ;
-function onTextareaInput(evt) {
-  const dataMessage = new FormData(evt.currentTarget);
-  let localStorageData = {};
-  dataMessage.forEach((value, name) => {
-      const obj = { [name]: value, };
-      Object.assign(localStorageData, obj); });
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localStorageData)); } 
-
+refs.form.addEventListener('input', throttle(evt => {
+    const formData = new FormData(evt.currentTarget);
+    let localStorageData = {};
+    formData.forEach((value, name) => {
+      const obj = {
+        [name]: value,
+      };
+      Object.assign(localStorageData, obj);
+    });
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localStorageData));
+  }, 500));
