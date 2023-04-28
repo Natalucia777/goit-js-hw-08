7b2cc0c8cc22a7be1d842380dbd63e509cc42332
Natalya Demidenko <natalucia777@ukr.net>
2023-04-28 22:24:50 +0300

    hw-8-3-4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,13 +3,21 @@ var throttle = require('lodash.throttle');
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('feedback-form textarea'),
+  input: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
 };
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', onTextareaInput);
+
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
 function onFormSubmit(evt) {
-  
-}
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
+  localStorage.removeItem(LOCAL_STORAGE_KEY); }
 function onTextareaInput(evt) {
-  
+  const message = evt.currentTarget.value;
+  console.log(message);
+  localStorage.setItem('feedback-form-state', message);
 }
